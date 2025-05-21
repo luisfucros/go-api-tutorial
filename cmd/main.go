16cee551a61a68ea3a84e92251fabf35646329af
Luis Felipe Salazar Ucros <40307832+luisfucros@users.noreply.github.com>
2025-05-21 15:38:01 +0000

    add db connection

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,43 @@ package main
 
 import (
 	"log"
+	"database/sql"
+	"github.com/go-sql-driver/mysql"
 	"github.com/luisfucros/go-api-tutorial/cmd/api"
+	"github.com/luisfucros/go-api-tutorial/db"
+	"github.com/luisfucros/go-api-tutorial/configs"
+
 )
 
 func main() {
-	server := api.NewAPIServer(":8080", nil)
-	err := server.Run(); if err != nil {
+	db, err := db.NewMySQLStorage(
+		mysql.Config{
+			User:                 configs.Envs.DBUser,
+			Passwd:               configs.Envs.DBPassword,
+			Addr:                 configs.Envs.DBAddress,
+			DBName:               configs.Envs.DBName,
+			Net:                  "tcp",
+			AllowNativePasswords: true,
+			ParseTime:            true,
+		},
+	)
+	if err != nil {
 		log.Fatal(err)
 	}
+
+	initStorage(db)
+
+	server := api.NewAPIServer(":8080", db)
+	err = server.Run(); if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func initStorage(db *sql.DB) {
+	err := db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("DB: Successfully connected!")
 }
