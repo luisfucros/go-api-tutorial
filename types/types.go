d3403ff50edd3343aaf0180abed74c7beba1b7cc
Luis Felipe Salazar Ucros <40307832+luisfucros@users.noreply.github.com>
2025-05-25 15:48:28 +0000

    update file

--- types/types.go
+++ types/types.go
@@ -25,20 +25,47 @@ type Product struct {
 	CreatedAt time.Time `json:"createdAt"`
 }
 
+type CartCheckoutItem struct {
+	ProductID int `json:"productID"`
+	Quantity  int `json:"quantity"`
+}
+
+type Order struct {
+	ID        int       `json:"id"`
+	UserID    int       `json:"userID"`
+	Total     float64   `json:"total"`
+	Status    string    `json:"status"`
+	Address   string    `json:"address"`
+	CreatedAt time.Time `json:"createdAt"`
+}
+
+type OrderItem struct {
+	ID        int       `json:"id"`
+	OrderID   int       `json:"orderID"`
+	ProductID int       `json:"productID"`
+	Quantity  int       `json:"quantity"`
+	Price     float64   `json:"price"`
+	CreatedAt time.Time `json:"createdAt"`
+}
+
 type UserStore interface {
 	GetUserByEmail(email string) (*User, error)
-	// GetUserByID(id int) (*User, error)
+	GetUserByID(id int) (*User, error)
 	CreateUser(User) error
 }
 
 type ProductStore interface {
 	GetProductByID(id int) (*Product, error)
-	// GetProductsByID(ids []int) ([]Product, error)
+	GetProductsByID(ids []int) ([]Product, error)
 	GetProducts() ([]*Product, error)
 	CreateProduct(CreateProductPayload) error
-	// UpdateProduct(Product) error
+	UpdateProduct(Product) error
 }
 
+type OrderStore interface {
+	CreateOrder(Order) (int, error)
+	CreateOrderItem(OrderItem) error
+}
 type CreateProductPayload struct {
 	Name        string  `json:"name" validate:"required"`
 	Description string  `json:"description"`
@@ -57,4 +84,8 @@ type RegisterUserPayload struct {
 type LoginUserPayload struct {
 	Email    string `json:"email" validate:"required,email"`
 	Password string `json:"password" validate:"required"`
+}
+
+type CartCheckoutPayload struct {
+	Items []CartCheckoutItem `json:"items" validate:"required"`
 }
